7d0fc3490ccff5340776ee57c1aedc30a9365595
Vivek7548 <deshmukhvivek7548>
2024-05-02 17:17:52 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: '65645ab7647c2dcf309fce088b117a35',
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
